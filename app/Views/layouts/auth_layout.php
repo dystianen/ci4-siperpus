c644a7a4bce370907b47f625d6e4a2c1cf6cba47
Dystian En <dystian.en@kelaspintar.id>
2024-01-28 23:34:18 +0700

    fix: auth layout

--- app/Views/layouts/auth_layout.php
+++ app/Views/layouts/auth_layout.php
@@ -38,17 +38,6 @@
       </div>
     </div>
   </div>
-
-  <script>
-    function toggleMenu() {
-      var menuItems = document.getElementsByClassName('menu-item');
-      for (var i = 0; i < menuItems.length; i++) {
-        var menuItem = menuItems[i];
-        menuItem.classList.toggle("hidden");
-      }
-    }
-  </script>
-
 </body>
 
 </html>
